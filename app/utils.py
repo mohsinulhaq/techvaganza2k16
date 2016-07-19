2087926c2a82e456f1b927704c18fd8a9c5f36f6
Mohsin Ul Haq <mohsinulhaq01@gmail.com>
2016-07-19 21:25:09 +0530

    use Flask-Assets with base template

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,53 @@
 from flask import make_response
 from functools import wraps, update_wrapper
 from datetime import datetime
+from flask_assets import Bundle, Environment
+from app import app
+
+assets = Environment(app)
+
+bundles = {
+    'landing_css': Bundle(
+        'css/index.css',
+        'css/font-awesome.min.css',
+        'css/comfortaa.css',
+        'css/lato.css',
+        filters='cssmin',
+        output='gen/landing_styles.css'),
+
+    'landing_js': Bundle(
+        'js/jquery.min.js',
+        'js/index.js',
+        'js/skrollr.min.js',
+        'js/skrollr.menu.min.js',
+        filters='jsmin',
+        output='gen/landing_scripts.js'),
+
+    'css': Bundle(
+        'css/bootstrap.min.css',
+        'css/component.css',
+        'css/animate.min.css',
+        'css/ct-paper.css',
+        'css/lato.css',
+        'css/examples.css',
+        'css/font-awesome.min.css',
+        'css/montserrat.css',
+        filters='cssmin',
+        output='gen/styles.css'),
+
+    'js': Bundle(
+        'js/jquery.min.js',
+        'js/index.js',
+        'js/jquery-ui-1.10.4.custom.min.js',
+        'js/bootstrap.min.js',
+        'js/classie.js',
+        'js/ct-paper.js',
+        filters='jsmin',
+        output='gen/scripts.js'),
+
+}
+
+assets.register(bundles)
 
 
 def nocache(view):
